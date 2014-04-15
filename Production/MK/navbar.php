f4a879c2f9649125982c43fecf589322b303ac2a
aceslaf <aleksandartrposki@gmail.com>
2014-04-15 14:16:45 +0200

    Services created

---
+++ Production/MK/navbar.php
@@ -0,0 +1,102 @@
+     
+<script type="text/javascript">
+   var navbarResizeFunct=function(){
+       var nWidth=$(window).width();   
+       
+      if(nWidth<768){             
+        $("#nava").show();
+        $("#bigmenu").hide();
+        return;
+      }
+      if((nWidth>=768)&&(nWidth<992)){             
+        $("#bigmenu").show();
+        $("#nava").hide();
+        $(".logoimg").css("left", (nWidth-10)/2 + "px");
+        return;
+      }else {
+        $("#bigmenu").show();
+        $("#nava").hide();
+        $(".logoimg").css("left", (nWidth-125)/2 + "px");
+
+      }        
+   };           
+   $(document).ready(navbarResizeFunct);         
+   $(window).resize(navbarResizeFunct);
+   
+</script>
+
+
+
+ <!-- LANGUAGES STRIPE -->
+  <div class="lang ">
+     <div class="container">
+        <div class="pull-right" style="margin-top:5px">
+           <a href="">MK</a> 
+           <a href="">EN</a>
+           <a href="">DE</a>
+        </div>
+     </div>
+  </div>
+
+
+  <!--NAVIGATION -->
+  <div class="stripe">
+  <div class="container">
+     <div id="bigmenu">
+        <img src="../images/logo1.png" class="logoimg"/>
+        <div class="navbar-collapse collapse">
+           <ul class="nav navbar-nav bigmenu">
+              <li class="active"><a href="home.html">Home</a></li>
+              <li class="dropdown">
+                 <a href="#" class="dropdown-toggle" data-toggle="dropdown">About ussr <b class="caret"></b></a>
+                 <ul class="dropdown-menu">
+                    <li><a href="aboutus1.html">What we do</a></li>
+                    <li><a href="aboutus2.html">Who we are</a></li>
+                    <li><a href="aboutus3.html">Team</a></li>
+                 </ul>
+              </li>
+              <li><a href="services.html" >Uslugi</a></li>
+              <li><a href="galery.html" style="background-image:none">Uslugi</a></li>
+           </ul>
+           <ul class="nav navbar-nav navbar-right bigmenu" style="margin-right:-5px">
+              <li><a href="#" style="background-color: rgb(165,182,42) !important; background-image:none;">Contact</a></li>
+           </ul>
+        </div>
+     </div>
+     <div id="nava">
+        <nav id="nav" class="navbar navbar-default" role="navigation">
+           <div class="container-fluid">
+              <!-- Brand and toggle get grouped for better mobile display -->
+              <div class="navbar-header">
+                 <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                 <span class="sr-only">Toggle navigation</span>
+                 <span class="icon-bar"></span>
+                 <span class="icon-bar"></span>
+                 <span class="icon-bar"></span>
+                 </button>
+                 <a class="navbar-brand  asdbackground" href="#">Летра</a>
+                 <!-- <img src="../images/logo.png" width="35" height="35" /> -->
+              </div>
+              <!-- Collect the nav links, forms, and other content for toggling -->
+              <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                 <ul class="nav navbar-nav navbar-right">
+                    <li><a href="#" class="activelink">Home</a></li>
+                    <li class="dropdown open">
+                       <a href="#" class="dropdown-toggle" data-toggle="dropdown">About us <b class="caret"></b></a>
+                       <ul class="dropdown-menu">
+                          <li><a href="#">Action</a></li>
+                          <li><a href="#">Another action</a></li>
+                          <li><a href="#">Something else here</a></li>
+                       </ul>
+                    </li>
+                    <li><a href="#">Uslugi</a></li>
+                    <li><a href="#" style="background-color: rgb(165,182,42) !important; background-image:none;">Contact</a></li>
+                 </ul>
+              </div>
+              <!-- /.navbar-collapse -->
+           </div>
+           <!-- /.container-fluid -->
+        </nav>
+     </div>
+  </div>
+ </div> 
---
+++ Production/MK/services.php
@@ -0,0 +1,188 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta name="description" content="">
+      <meta name="author" content="">
+      <link rel="shortcut icon" href="../../assets/ico/favicon.ico">
+      <title>Letra</title>
+      <link rel="shortcut icon" href="../images/logo1.png" />
+      <!--bootstrap-->
+      <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+      <link rel="stylesheet" href="http://code.jquery.com/ui/1.10.4/themes/smoothness/jquery-ui.css">
+      <script src="http://code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
+      <link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.css">
+      <link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap-theme.css">
+      <script src="http://netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+      <link href="../css/jubmatron.css" rel="stylesheet">
+      <!--end bootstrap-->
+      <link href="../css/reset.css" rel="stylesheet">
+      <link href="../css/main.css" rel="stylesheet">
+
+
+        
+
+      </style>
+      <link rel="stylesheet" href="http://code.jquery.com/ui/1.10.4/themes/smoothness/jquery-ui.css">
+        <script src="http://code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
+        <style>
+           .acrd >h3{
+             border: none !important; 
+           border-radius: 0 !important;
+           background: none;
+           background-color: /*#eaeade*/ #a5b62a !important;
+           color: white !important;
+           font-weight: bold;
+         }
+        </style>
+        <script>
+              $(function() {
+                $( ".acrd" ).accordion({
+                  collapsible: true,
+                active:false,
+                icons:false,
+                heightStyle: "content"
+                });
+              });
+        </script>
+
+
+      
+      <link href="../css/reset.css" rel="stylesheet">
+   </head>
+   <body>
+     <?php require_once('navbar.php'); ?>
+
+
+
+
+
+
+     <!--CONTENT -->
+     <div class="containter">
+     <style type="text/css">
+        .containter .row{
+          width: 90%;
+          margin: 50px auto 0 auto;
+        }
+        .uslugiimg{
+          margin: 0 auto -20px auto;
+          width: 80%;
+          height: 66px;
+          max-width: 547px;
+        }
+        .uslugiimg img{
+          width: 100%;
+        }
+
+        .row .col-sm-4 {
+          margin-top:40px;
+        }
+
+        .row .acrd{
+          padding-left:10px;
+          padding-right: 10px;
+        }
+
+        .uslugatitle{
+          font-size: 18px;
+          font-weight: bold;
+          margin-bottom: 10px;
+        }
+
+     </style>
+     <div class="row">
+        <div class="uslugiimg"><img src="../images/uslugi.png"/></div>
+        <div class="col-sm-4  ">
+           <div class="uslugatitle">USLUGI1</div>    
+           <div class="acrd">
+                  <h3 style="border: none; border-radius: 0;">Section 1</h3>
+                  <div>
+                    <p>
+                  cipit eget, quam. Integer ut neque. Vivamus nisi metus, molestie vel, gravida in, condimentum sit amet, nunc. Nam a nibh. Donec suscipit eros. Nam mi. Proin viverra leo ut odio. Curabitur malesuada. Vestibulum a velit eu ante scelerisque vulputate</p>
+                  </div>
+                  <h3>Section 2</h3>
+                  <div>
+                    <p>Sed non urna. Donec et ante. Phasellus eu ligula. Vestibulum sit amet purus. Vivamus hendrerit, dolor at aliquet laoreet, mauris turpis porttitor velit, faucibus interdum tellus libero ac justo. Vivamus non quam. In suscipit faucibus urna. </p>
+                  </div>
+                  <h3>Section 3</h3>
+                  <div>
+                    <p>Nam enim risus, molestie et, porta ac, aliquam ac, risus. Quisque lobortis. Phasellus pellentesque purus in massa. Aenean in pede. Phasellus ac libero ac tellus pellentesque semper. Sed ac felis. Sed commodo, magna quis lacinia ornare, quam ante aliquam nisi, eu iaculis leo purus venenatis dui. </p>                    
+                  </div>
+                  <h3>Section 4</h3>
+                  <div>
+                    <p>Cras dictum. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aenean lacinia mauris vel est. </p><p>Suspendisse eu nisl. Nullam ut libero. Integer dignissim consequat lectus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. </p>
+                  </div>
+          </div>
+        </div>
+        <div class="col-sm-4">
+          <div class="uslugatitle">USLUGI2</div>    
+          <div class="acrd">
+                  <h3 style="border: none; border-radius: 0;">Section 1</h3>
+                  <div>
+                    <p>
+                  cipit eget, quam. Integer ut neque. Vivamus nisi metus, molestie vel, gravida in, condimentum sit amet, nunc. Nam a nibh. Donec suscipit eros. Nam mi. Proin viverra leo ut odio. Curabitur malesuada. Vestibulum a velit eu ante scelerisque vulputate</p>
+                  </div>
+                  <h3>Section 2</h3>
+                  <div>
+                    <p>Sed non urna. Donec et ante. Phasellus eu ligula. Vestibulum sit amet purus. Vivamus hendrerit, dolor at aliquet laoreet, mauris turpis porttitor velit, faucibus interdum tellus libero ac justo. Vivamus non quam. In suscipit faucibus urna. </p>
+                  </div>
+                  <h3>Section 3</h3>
+                  <div>
+                    <p>Nam enim risus, molestie et, porta ac, aliquam ac, risus. Quisque lobortis. Phasellus pellentesque purus in massa. Aenean in pede. Phasellus ac libero ac tellus pellentesque semper. Sed ac felis. Sed commodo, magna quis lacinia ornare, quam ante aliquam nisi, eu iaculis leo purus venenatis dui. </p>                    
+                  </div>
+                  <h3>Section 4</h3>
+                  <div>
+                    <p>Cras dictum. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aenean lacinia mauris vel est. </p><p>Suspendisse eu nisl. Nullam ut libero. Integer dignissim consequat lectus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. </p>
+                  </div>
+          </div>  
+        </div> 
+        <div class="col-sm-4">
+          <div class="uslugatitle">USLUGI3</div>    
+          <div class="acrd">
+                  <h3 style="border: none; border-radius: 0;">Section 1</h3>
+                  <div>
+                    <p>
+                  cipit eget, quam. Integer ut neque. Vivamus nisi metus, molestie vel, gravida in, condimentum sit amet, nunc. Nam a nibh. Donec suscipit eros. Nam mi. Proin viverra leo ut odio. Curabitur malesuada. Vestibulum a velit eu ante scelerisque vulputate</p>
+                  </div>
+                  <h3>Section 2</h3>
+                  <div>
+                    <p>Sed non urna. Donec et ante. Phasellus eu ligula. Vestibulum sit amet purus. Vivamus hendrerit, dolor at aliquet laoreet, mauris turpis porttitor velit, faucibus interdum tellus libero ac justo. Vivamus non quam. In suscipit faucibus urna. </p>
+                  </div>
+                  <h3>Section 3</h3>
+                  <div>
+                    <p>Nam enim risus, molestie et, porta ac, aliquam ac, risus. Quisque lobortis. Phasellus pellentesque purus in massa. Aenean in pede. Phasellus ac libero ac tellus pellentesque semper. Sed ac felis. Sed commodo, magna quis lacinia ornare, quam ante aliquam nisi, eu iaculis leo purus venenatis dui. </p>                    
+                  </div>
+                  <h3>Section 4</h3>
+                  <div>
+                    <p>Cras dictum. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aenean lacinia mauris vel est. </p><p>Suspendisse eu nisl. Nullam ut libero. Integer dignissim consequat lectus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. </p>
+                  </div>
+          </div>
+        </div>
+       </div> 
+      </div>
+     </div>
+     
+
+
+
+
+
+
+
+
+
+
+
+
+     <!-- FOOOTER-->
+        <!-- FOOOTER-->
+   <div class="footer bggreen ">   
+       <div class="container">
+         <p>@Letra 2014</p>
+       </div>
+   </div>
+   </body>
+</html>
