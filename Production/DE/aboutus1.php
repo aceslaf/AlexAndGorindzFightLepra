58f4f183c9ceee1aaf27b602f896aa3501a9e31a
aceslaf <aleksandartrposki@gmail.com>
2014-04-27 17:25:47 +0200

    Closed open one line tags

--- Production/DE/aboutus1.php
+++ Production/DE/aboutus1.php
@@ -2,24 +2,24 @@
 <html lang="en">
    <head>
       <meta charset="utf-8">
-      <meta http-equiv="X-UA-Compatible" content="IE=edge">
-      <meta name="viewport" content="width=device-width, initial-scale=1">
-      <meta name="description" content="">
-      <meta name="author" content="">
-      <link rel="shortcut icon" href="../../assets/ico/favicon.ico">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+      <meta name="viewport" content="width=device-width, initial-scale=1"/>
+      <meta name="description" content=""/>
+      <meta name="author" content=""/>
+      <link rel="shortcut icon" href="../../assets/ico/favicon.ico"/>
       <title>Letra</title>
       <link rel="shortcut icon" href="../images/logo1.png" />
       <!--bootstrap-->
       <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-      <link rel="stylesheet" href="http://code.jquery.com/ui/1.10.4/themes/smoothness/jquery-ui.css">
+      <link rel="stylesheet" href="http://code.jquery.com/ui/1.10.4/themes/smoothness/jquery-ui.css"/>
       <script src="http://code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
-      <link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.css">
-      <link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap-theme.css">
+      <link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.css"/>
+      <link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap-theme.css"/>
       <script src="http://netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
-      <link href="../css/jubmatron.css" rel="stylesheet">
+      <link href="../css/jubmatron.css" rel="stylesheet"/>
       <!--end bootstrap-->
-      <link href="../css/reset.css" rel="stylesheet">
-      <link href="../css/main.css" rel="stylesheet">
+      <link href="../css/reset.css" rel="stylesheet"/>
+      <link href="../css/main.css" rel="stylesheet"/>
 
       <script type="text/javascript">
          $(document).ready(function(){
