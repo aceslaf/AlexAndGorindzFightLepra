de84d2133005788f713be1e7adfd3423b0ed88fd
aceslaf <aleksandartrposki@gmail.com>
2014-08-16 15:10:20 +0200

    Content hover added to index

--- Production/DE/index.php
+++ Production/DE/index.php
@@ -25,32 +25,43 @@
       <link rel="stylesheet" href="../owl-carousel/owl.carousel.css"> 
 	  <link rel="stylesheet" href="../owl-carousel/owl.theme.css">
 	  <script src="../owl-carousel/owl.carousel.js"></script>
-
+	  <script src="../js/contentHover.js"></script>
+      
 
       <script type="text/javascript">
          $(document).ready(function(){
 			fixPicture();
 			makeCarousel();
+			makeContentHover();
          });
          
          $(window).resize(function(){
 			fixPicture();
          });
+        
+        function makeContentHover(){
+              $('.contentNoHover').contenthover({
+              	  data_selector: '.contentHover',
+			      overlay_background:'#000',
+			      overlay_opacity:0.8
+			  });
+
+        }
 
         function fixPicture(){
            var widthVrzHeight=2.13178294574; 
            var nWidth=$(window).width();   
            if(nWidth<768){
-             $(".headerpic>img").attr("width",nWidth);
+             $(".headerpic img").attr("width",nWidth);
              return;
            }
             if(nWidth<1400){
-              $(".headerpic>img").attr("width",825);	
+              $(".headerpic img").attr("width",825);	
               return;
             }
 
             if(nWidth>=1400){
-               $(".headerpic>img").attr("width",Math.floor(nWidth*0.55));	
+               $(".headerpic img").attr("width",Math.floor(nWidth*0.55));	
               return;	
             }
           };    
@@ -80,7 +91,7 @@
 		      // itemsTablet: false,
 		      // itemsMobile : false
 		 
-		  });};               
+		    });};               
       </script>
       <style type="text/css">
           .item{
@@ -98,17 +109,37 @@
      <div class="row headerstuff">
       
         <div class="headerpic col-sm-6  ">
+        <div class="contentNoHover">
           <img src="http://alldaycreative.co.uk/wp-content/themes/allday/images/new-slide-one.jpg"/>
+        </div>
+        <div class="contentHover">
+          <div class="item">
+	              
+					 <h1 class="fancyTitleFont colorGreen" style="
+                                font-family: 'proxima-nova', 'Helvetica Neue', Helvetica, Arial, sans-serif;
+                                color: rgb(165,182,42);
+                                text-align: center;" >
+						Mislam deka naslov e pozelen
+					 </h1>
+					 <p class="defaultFont"  style="font-family: "Bariol_Regular", "Helvetica Neue", Helvetica, Arial, sans-serif;
+                                color: #999;
+                                text-align: justify; ">
+					 Letra Translations ist Ihr Übersetzungsbüro in Mazedonien. Fachübersetzungen, beglaubigte Dokumenten, Konferenzdolmetschen -wir sind spezialisiert für Deutsch, English, Spanisch und Mazedonisch, aber sehen Sie sich die anderen Sprachkombinationen die wir anbieten.
+					 </p>
+				   </div>
+           </div>
+          
         </div>
         <div class="col-sm-6 .defaultFont">
             <div class="contentHolder bigMessage" >
 			 <div id="owl-demo" class="owl-carousel owl-theme" >
 	               <div class="item">
 	              
-					 <h1 class="fancyTitleFont colorGreen" >
+					 <h1 class="fancyTitleFont colorGreen"  >
 						Normal ist überbewertet
 					 </h1>
-					 <p class="defaultFont">
+					 <p class="defaultFont"
+					    >
 					  Wir sind etwas besonderes!
 					 </p>
 				   </div>	 
